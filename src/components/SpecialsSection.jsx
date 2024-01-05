7159a619c57af2249526234d9ae6b7464527a746
LuisGutWo <agutierrezwong@gmail.com>
2024-01-05 02:11:09 -0400

    feat (specials): usetranslation

--- src/components/SpecialsSection.jsx
+++ src/components/SpecialsSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Nav, Tab } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 import SpecialImg1 from "../assets/img/OIP.jpeg";
 import SpecialImg2 from "../assets/img/cocina-bistro-tagliolini.jpg";
@@ -11,12 +12,13 @@ import "aos/dist/aos.css";
 AOS.init();
 
 const SpecialsSection = () => {
+  const [t] = useTranslation("global");
   return (
     <section id="specials" className="specials">
       <Container className="container" data-aos="fade-up">
         <div className="section-title">
-          <h2>lo nuevo de esta temporada</h2>
-          <p>La selección del Chef de esta semana...</p>
+          <h2>{t("specials.title")} </h2>
+          <p>{t("specials.paragraph")} </p>
         </div>
 
         <Tab.Container defaultActiveKey="tab-1">
@@ -25,17 +27,17 @@ const SpecialsSection = () => {
               <Nav variant="pills" className="flex-column">
                 <Nav.Item>
                   <Nav.Link eventKey="tab-1">
-                    Chitarra al Pesto di Sorrento
+                    {t("specials.cards.item_1.title")}
                   </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                   <Nav.Link eventKey="tab-2">
-                    Spaghetti Pomodoro dei tre re
+                    {t("specials.cards.item_2.title")}
                   </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                   <Nav.Link eventKey="tab-3">
-                    Burrata con oleo al carbone
+                    {t("specials.cards.item_3.title")}
                   </Nav.Link>
                 </Nav.Item>
               </Nav>
@@ -45,14 +47,12 @@ const SpecialsSection = () => {
                 <Tab.Pane eventKey="tab-1">
                   <div className="row">
                     <div className="col-lg-8 details order-2 order-lg-1">
-                      <h3>Chitarra al Pesto di Sorrento</h3>
+                      <h3>{t("specials.cards.item_1.title")}</h3>
                       <p className="fst-italic">
-                        Fresco pesto a base de almendras blancas y limón sutil.
-                        Servidos en pasta seca Chitarra y terminado con
-                        almendras tostadas.
+                        {t("specials.cards.item_1.paragraph")}
                       </p>
                     </div>
-                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                    <div className="col-lg-4 text-center img-container order-1 order-lg-2">
                       <img src={SpecialImg1} alt="" className="img-fluid" />
                     </div>
                   </div>
@@ -60,14 +60,12 @@ const SpecialsSection = () => {
                 <Tab.Pane eventKey="tab-2">
                   <div className="row">
                     <div className="col-lg-8 details order-2 order-lg-1">
-                      <h3>Spaghetti Pomodoro dei tre re</h3>
+                      <h3>{t("specials.cards.item_2.title")}</h3>
                       <p className="fst-italic">
-                        Pasta larga seca Mancini, aromatizado con ajos, tomates
-                        daterinos salteados en aceite EVO y en salsa de tres
-                        tomates Italianos.
+                        {t("specials.cards.item_2.paragraph")}
                       </p>
                     </div>
-                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                    <div className="col-lg-4 text-center img-container order-1 order-lg-2">
                       <img src={SpecialImg2} alt="" className="img-fluid" />
                     </div>
                   </div>
@@ -75,15 +73,12 @@ const SpecialsSection = () => {
                 <Tab.Pane eventKey="tab-3">
                   <div className="row">
                     <div className="col-lg-8 details order-2 order-lg-1">
-                      <h3>Burrata con oleo al carbone</h3>
+                      <h3>{t("specials.cards.item_3.title")}</h3>
                       <p className="fst-italic">
-                        Queso fresco de leche de vaca, hongos boletus
-                        confitados, laminas de guanciale templado, tomates semi
-                        secos y terminados con aceite aromatizado al carbon y
-                        ahumado con madera de cerezo.
+                        {t("specials.cards.item_3.paragraph")}
                       </p>
                     </div>
-                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                    <div className="col-lg-4 text-center img-container order-1 order-lg-2">
                       <img src={SpecialImg3} alt="" className="img-fluid" />
                     </div>
                   </div>
